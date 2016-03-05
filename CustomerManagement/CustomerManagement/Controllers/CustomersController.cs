5bf748637f66503388eed1b8a3388e3fb549b95d
Michał Sakwa <saka1988@gmail.com>
2016-03-05 20:55:06 +0100

    Added base controllers. Added references to EF.

---
+++ CustomerManagement/CustomerManagement/Controllers/CustomersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace CustomerManagement.Controllers
+{
+    [RoutePrefix("api/customers")]
+    public class CustomersController : ApiController
+    {
+
+        public async Task<IHttpActionResult> GetCustomers()
+        {
+
+            return Ok();
+        }
+    }
+}
